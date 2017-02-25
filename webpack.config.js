89c066cb29d692dee2d34b7541dead665ea696fd
Terry O'Shea <terrykoshea@gmail.com>
2017-02-24 19:51:37 -0800

    minify js files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
-module.exports = {
+const webpack = require('webpack');
+
+const config = {
   entry: "./main.js",
   output: {
     path: __dirname,
     filename: "bundle.js"
-  }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
 };
+
+module.exports = config;
